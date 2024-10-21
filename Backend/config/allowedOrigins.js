044de1cd7a73784b6c1df3ae697d9eaddf93394b
lokeshsharma-glx <lokesh.sharma@globixo.com>
2024-10-21 22:09:01 +0530

    modified CORS allowed origins

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,9 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:5174",
-  "http://localhost:5173",
-  "https://www.example.com",
-  "https://example.com",
-  "https://10xdevchallenge.netlify.app/",
-];
+const allowedOrigins = ["https://10xdevchallenge.netlify.app"];
 
 module.exports = allowedOrigins;
