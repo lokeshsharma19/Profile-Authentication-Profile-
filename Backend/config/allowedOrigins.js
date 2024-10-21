6ecf6d4711fe503c9c79a03bfa15d0fce6ca4e93
lokeshsharma-glx <lokesh.sharma@globixo.com>
2024-10-21 21:05:30 +0530

    updated allowed origins

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://www.example.com",
   "https://example.com",
+  "https://10xdevchallenge.netlify.app/sign-up",
 ];
 
 module.exports = allowedOrigins;
